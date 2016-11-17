1fd49a503cc87022e27df2afdae21c7711cbf480
ve-2016 <jan.kowalski.inf@gmail.com>
2016-11-17 18:12:04 +0100

    'explorers'

---
+++ DockProjects/Form1.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace DockProjects
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
